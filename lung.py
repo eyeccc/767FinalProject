ae55ec37a941f86a78f2d2559dcb1c380bd93f69
Chih-Ching Chang <cchang83@wisc.edu>
2016-11-07 16:14:17 -0600

    read binary image data

--- lung.py
+++ lung.py
@@ -1,6 +1,12 @@
 import numpy as np
 from matplotlib import pylab as plt
 
-A = np.fromfile("JPCLN138.img", dtype='int16', sep="")
+A = np.fromfile("JPCLN138.IMG", dtype='int16', sep="")
+
+#with open('JPCLN138.IMG', 'rb') as f:
+#  data = f.read(16)
+#  text = data.decode('hex')
+#  print text
 A = A.reshape([2048, 2048])
 plt.imshow(A)
+plt.show()
