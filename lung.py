ae97861ec1fa2001547965a19ac9f8de2f63f50a
Chih-Ching Chang <cchang83@wisc.edu>
2016-11-08 09:48:36 -0600

    test theano import

--- lung.py
+++ lung.py
@@ -1,6 +1,11 @@
 import numpy as np
 from matplotlib import pylab as plt
+# this part is just for bypassing the authorization
+# of installing new python package in lab machine
+import sys
+sys.path.append("/u/c/h/chih-ching/Theano")
 
+import theano
 A = np.fromfile("JPCLN138.IMG", dtype='int16', sep="")
 
 #with open('JPCLN138.IMG', 'rb') as f:
