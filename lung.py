de4cc1effb68382cb9e8c4d47653b971557ec871
TimTu <cchang83@wisc.edu>
2016-11-18 15:03:21 -0500

    test changing config user email

--- lung.py
+++ lung.py
@@ -21,19 +21,28 @@ from keras.preprocessing import image
 from imagenet_utils import preprocess_input, decode_predictions
 # path for images 
 # /Users/waster/Downloads/All247images
-def readimg(prestr, minNum, maxNum):
-  filename = prestr
-  out = []
-  for i in range(minNum, maxNum):
-    name = filename + str(maxNum).zfill(3) + ".IMG"
-    A = np.fromfile(name, dtype='int16', sep="")
-    #A = A.reshape([2048, 2048])
-    A = np.resize(A,[224,224])
-    B = np.repeat(A[:,:,np.newaxis],3,axis=2)
-    #A = A.reshape([1,3,1182,1182])
-    out.append(B)
+def getFeatures(path, prestr, idx, model):
+  img = readimg(path + prestr, idx)
+  x = image.img_to_array(img)
+  x = np.expand_dims(x, axis=0)
+  x = preprocess_input(x)
+  layer_list = [5,31,34,70,73,132,135,174]
+  feat_list = []
+  for i in range(0, len(layer_list)):
+    feat = get_activations(model, i, x)
+    feat_list.append(feat)
+  return feat_list
 
-  return out
+def readimg(prestr, idx):
+  filename = prestr
+  name = filename + str(idx).zfill(3) + ".IMG"
+  A = np.fromfile(name, dtype='int16', sep="")
+    #A = A.reshape([2048, 2048])
+  A = np.resize(A,[224,224])
+  B = np.repeat(A[:,:,np.newaxis],3,axis=2)
+    #A = A.reshape([1,3,1182,1182])
+  return B
+
 def get_activations(model, layer_idx, X_batch):
 #  print(model.layers[layer_idx].get_config())
   get_activations = K.function([model.layers[0].input, K.learning_phase()], [model.layers[layer_idx].output,])
@@ -45,38 +54,37 @@ def main():
   #nodule_img = readimg("JPCLN", 0, 154)
   #non_nodule_img = readimg("JPCNN", 0, 93)
   model = ResNet50(weights='imagenet')
-  nodule_img = readimg("JPCLN",137,138)
+
+  pos_feat = []
+  path = "/Users/waster/Downloads/All247images/"
+  prestr = "JPCLN"
+  for i in range(1,10):
+    f = getFeatures(path, prestr, i, model)
+    pos_feat.append(f)
+
+  neg_feat = []
+  prestr = "JPCNN"
+  for i in range(1,10):
+    f = getFeatures(path, prestr, i, model)
+    neg_feat.append(f)
+
   #plt.imshow(nodule_img[0],cmap='Greys_r')
   #plt.show()
   #print (nodule_img)
   #print ("test")
   #img_path = 'elephant.jpg'
   #img = image.load_img(img_path, target_size=(224, 224))
-  img = nodule_img[0]
-# print(img.shape)
-  x = image.img_to_array(img)
-#print(x.shape)
-  x = np.expand_dims(x, axis=0)
-  x = preprocess_input(x)
-  #print(model.layers[3].__dict__)
-  layer_list = [5,31,34,70,73,132,135,174]
-  all_feat = []
-  feature_list = []
-  feat = []
-  for i in range(0, len(layer_list)):
-    feat = get_activations(model, 5, x)
-    feature_list.append(feat)
-  all_feat.append(feature_list)
+  
   
 #theano.function([model.layers[0].input], convout1.get_output(train=False), allow_input_downcast=False)
 #feat = get_feature(x)
 #plt.imshow(feat)
 # max: four dim array
-  print(len(feat))
-  print(len(feat[0]))
-  print(len(feat[0][0]))
-  print(len(feat[0][0][0]))
-  print(len(feat[0][0][0][0]))
+#  print(len(feat))
+#  print(len(feat[0]))
+#  print(len(feat[0][0]))
+#  print(len(feat[0][0][0]))
+#  print(len(feat[0][0][0][0]))
 #preds = model.predict(x)
 # print('Predicted:', decode_predictions(preds))
 
