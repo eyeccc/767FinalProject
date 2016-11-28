fb81259a6812508efcbf024850c7f219eb05728e
Chih-Ching Chang <ching2032@gmail.com>
2016-11-28 13:23:19 -0600

    better contrast

--- preprocess.m
+++ preprocess.m
@@ -1,24 +1,28 @@
 function out = preprocess(A)
   A = imcomplement(A);
   B = medfilt2(A);
+ % B = imadjust(B,stretchlim(B),[]);
   B = highboost(B);
   %imshow(B);
-  %B = imadjust(B,stretchlim(B),[]);
-  J = histeq(B);
+  
+  %J = histeq(B);
+  J = adapthisteq(B);
   %imshow(J);
   J = medfilt2(J);
   %imshow(J);
   J = imadjust(J,stretchlim(J),[]);
+  %J = highboost(J);
   J = imcomplement(J);
-  %imshow(J);
+  % R = imsharpen(J);
+  %imshow(R);
   out = J;%im2bw(J, graythresh(J));
 end
 
 function o = highboost(img)
-  m = [1 1 1; 1 1 1; 1 1 1]/9;
-  f = imfilter(img, m);
+ % m = [1 1 1; 1 1 1; 1 1 1]/9;
+  %f = imfilter(img, m);
   k = 4;
-  u = [-1 -1 -1;-1 k+4 -1; -1 -1 -1];
-  fh = imfilter(img,u) + f;
+  u = [0 -k 0;-k 4*k+1 -k; 0 -k 0];
+  fh = imfilter(img,u,'same');% + f;
   o = fh;
 end
