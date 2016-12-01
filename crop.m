954b90207f151fb5c1202483c924371b11473383
Chih-Ching Chang <ching2032@gmail.com>
2016-12-01 14:35:29 -0600

    patches

--- crop.m
+++ crop.m
@@ -2,8 +2,18 @@ M = csvread('pos_and_bm.csv');
 for i = 1:1:154
 infile = strcat('D:\上課相關\Fall 2016\767 Medical Image Analysis\processed_img\pp', num2str(i, '%03d'), '.png');
 im = imread(infile);
-out = im(M(i,2)-127:M(i,2)+128, M(i,1)-127:M(i,1)+128);
+while(1)
+    r = floor(rand*2048);
+    c = floor(rand*2048);
+    if ((r +127 < M(i,2)-127 || r -128 > M(i,2)+128) && ...
+        (c+127 < M(i,1)-127 || c-128 > M(i,1)+128) && ...
+        (r-127 >= 1) && (r+128 <= 2048) && (c-127 >= 1) && (c+128 <= 2048))
+        out = im(r-127:r+128, c-127:c+128);
+        break;
+    end
+end
+%out = im(M(i,2)-127:M(i,2)+128, M(i,1)-127:M(i,1)+128);
 %disp(size(crop_im));
-outfile = strcat('D:\上課相關\Fall 2016\767 Medical Image Analysis\cropped_img\c', num2str(i, '%03d'), '.png');
+outfile = strcat('D:\上課相關\Fall 2016\767 Medical Image Analysis\cropped_img\npatchp', num2str(i, '%03d'), '.png');
 imwrite(out,outfile);
 end
